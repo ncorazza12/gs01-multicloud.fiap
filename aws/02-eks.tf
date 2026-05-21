a33f124de052309906eff2923c0c04135838dea2
luizbrito7 <luizbrito.x@gmail.com>
2026-05-20 23:47:17 -0300

    feat(aws): add eks cluster

---
+++ aws/02-eks.tf
@@ -0,0 +1,34 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "eks-${local.project}-${local.env}"
+  kubernetes_version = "1.33"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
+
+  endpoint_public_access = true
+
+  addons = {
+    coredns                = {}
+    kube-proxy             = {}
+    vpc-cni                = { before_compute = true }
+    eks-pod-identity-agent = { before_compute = true }
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.small"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+    }
+  }
+}
+
+output "configure_kubectl" {
+  value = "aws eks update-kubeconfig --region eu-west-2 --name ${module.eks.cluster_name}"
+}
