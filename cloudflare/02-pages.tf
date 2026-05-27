e4f2cf98f85aa124741832b97ebed745c39d9dba
luizbrito7 <luizbrito.x@gmail.com>
2026-05-27 15:37:11 -0300

    feat(infra): connect pages project to github source

--- cloudflare/02-pages.tf
+++ cloudflare/02-pages.tf
@@ -3,6 +3,18 @@ resource "cloudflare_pages_project" "frontend" {
   name              = "gs01-${local.project}-${local.env}"
   production_branch = "main"
 
+  source {
+    type = "github"
+    config {
+      owner                         = "luizbrito7"
+      repo_name                     = "gs01-multicloud.fiap"
+      production_branch             = "main"
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_deployment_setting    = "all"
+    }
+  }
+
   build_config {
     build_command   = "npm run build"
     destination_dir = "dist"
